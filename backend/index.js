8829c243fb654da8872bfb27e93763f61391054e
Nagul71 <118661118+Nagul71@users.noreply.github.com>
2025-01-23 01:15:53 +0530

    commit changes

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ import {ClerkExpressRequireAuth} from '@clerk/clerk-sdk-node'
 const port = process.env.PORT || 3000
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true,  
