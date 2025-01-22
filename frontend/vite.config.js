a7f52cb3a8c526f9412a39ae6d330e05a36de008
Nagul71 <118661118+Nagul71@users.noreply.github.com>
2025-01-22 20:45:33 +0530

    updated env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Bind to all IP addresses on the local network
-    port: 5173,      // Specify the port (keep the default 5173)
-  },
 })
